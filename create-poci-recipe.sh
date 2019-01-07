cc41aca02113ebc1f1d82675d46e3737b61456bd
Paul Vidal <paul.vid@gmail.com>
2019-01-07 09:51:26 -0500

    Script Review

---
+++ create-poci-recipe.sh
@@ -0,0 +1,2 @@
+rm -rf poci-bmq-data-science.sh >/dev/null 2>&1
+echo "mysql -u bmq_user -pBe@stM0de beast_mode_db --execute=\"""$(mysqldump -u bm_user -pHWseftw33# beast_mode_db 2> /dev/null)""\"" >> poci-bmq-data-science.sh 
