6fb12ca6193ae68321b4aa69930ff5e67ef4d497
Paul Vidal <paul.vid@gmail.com>
2019-01-07 10:01:37 -0500

    Script Review

--- create-poci-recipe.sh
+++ create-poci-recipe.sh
@@ -1,3 +1,3 @@
 rm -rf poci-bmq-data-science.sh >/dev/null 2>&1
-echo "mysql -u bmq_user -pBe@stM0de beast_mode_db --execute=\"""$(mysqldump -u bm_user -pHWseftw33# beast_mode_db 2> /dev/null | sed s/\`//g`)""\"" >> poci-bmq-data-science.sh 
+echo "mysql -u bmq_user -pBe@stM0de beast_mode_db --execute=\"""$(mysqldump -u bm_user -pHWseftw33# beast_mode_db 2> /dev/null | sed s/\`//g)""\"" >> poci-bmq-data-science.sh 
 chmod a+x poci-bmq-data-science.sh 
--- launch-bmq-ephemeral-cluster.sh
+++ launch-bmq-ephemeral-cluster.sh
@@ -11,7 +11,7 @@ export PATH=$PATH:/Users/pvidal/Documents/Playground/cb-cli/
 ###################################################
 
 rm -rf poci-bmq-data-science.sh >/dev/null 2>&1
-echo "mysql -u bmq_user -pBe@stM0de beast_mode_db --execute=\"""$(mysqldump -u bm_user -pHWseftw33# beast_mode_db 2> /dev/null | sed s/\`//g`)""\"" >> poci-bmq-data-science.sh 
+echo "mysql -u bmq_user -pBe@stM0de beast_mode_db --execute=\"""$(mysqldump -u bm_user -pHWseftw33# beast_mode_db 2> /dev/null | sed s/\`//g)""\"" >> poci-bmq-data-science.sh 
 chmod a+x poci-bmq-data-science.sh 
 
 ##################################
